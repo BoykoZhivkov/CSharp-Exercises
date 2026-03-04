2f189e9f5e279be60545e0115d158ced4f6a8dff
Boyko Zhivkov <boykozhivkov@Boykos-MacBook-Air.local>
2026-03-04 18:01:11 +0200

    Add 4 C# Exercises

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+if (IsPrime(x))
+{
+    Console.WriteLine("Prime");
+} else 
+{
+    Console.WriteLine("Not Prime");
+}
+
+bool IsPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+Console.WriteLine(CalculateSum(n));
+
+int CalculateSum(int num)
+{
+    int result = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+
+    return result;
+}
